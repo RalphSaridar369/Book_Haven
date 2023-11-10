aa14199b353eab01c1a217f909e7d246e4b1cbc0
RalphSaridar369 <ralphsaridar@hotmail.com>
2023-11-10 17:52:29 +0200

    first commit

---
+++ components/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./header.css">
+    <link rel="stylesheet" href="../index.css">
+</head>
+
+<body>
+    <div class="header_container">
+        <img src="../images/Logo.png" alt="logo" class="logo_image">
+        <div>
+            <a href="/login.php">Login</a>
+            <span style="color:white">|</span>
+            <a href="/register.php">Register</a>
+        </div>
+    </div>
+</body>
+
+</html>

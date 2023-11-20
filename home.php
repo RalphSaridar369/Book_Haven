07c9751e2ed2de8e72b8351428508b286475898c
RalphSaridar369 <ralphsaridar@hotmail.com>
2023-11-20 18:59:01 +0200

    working on home page

--- home.php
+++ home.php
@@ -33,6 +33,47 @@
 
             }
         }
+
+        // document.addEventListener('DOMContentLoaded', function() {
+        //     var fadeElements = document.querySelectorAll('.fade-elements');
+        //     var fadeElements2 = document.querySelectorAll('.fade-elements-details');
+
+        //     fadeElements.forEach(function(element) {
+        //         element.addEventListener('mouseenter', function() {
+        //             element.classList.remove('active');
+        //             element.classList.add('inactive');
+        //         });
+
+        //         element.addEventListener('mouseleave', function() {
+        //             element.classList.remove('inactive');
+        //             element.classList.add('active');
+        //         });
+        //     });
+
+        //     fadeElements2.forEach(function(element) {
+        //         element.addEventListener('mouseenter', function() {
+        //             element.classList.add('active');
+        //             element.classList.remove('inactive');
+        //         });
+
+        //         element.addEventListener('mouseleave', function() {
+        //             element.classList.add('inactive');
+        //             element.classList.remove('active');
+        //         });
+        //     });
+        // });
+
+        function showDetails(element) {
+            element.style.opacity = '0';
+            element.nextElementSibling.style.opacity = '1';
+        }
+
+
+
+        function hideDetails(element) {
+            element.style.opacity = '1';
+            element.nextElementSibling.style.opacity = '0';
+        }
     </script>
 </head>
 
@@ -51,11 +92,36 @@
         <?php
         for ($i = 0; $i < 5; $i++) {
             echo '
-            <img src="./images/booksForHome/1.jpg" class="product_image" />
-            <img src="./images/booksForHome/2.jpg" class="product_image" />
-            <img src="./images/booksForHome/3.jpg" class="product_image" />
-            <img src="./images/booksForHome/4.jpg" class="product_image" />
-            <img src="./images/booksForHome/5.jpg" class="product_image" />';
+            <div class="product_container">
+                <img src="./images/booksForHome/1.jpg" class="product_image fade-elements" onmouseenter="showDetails(this)" onmouseleave="hideDetails(this)" />
+                <div class="fade-elements-details">
+                    <p style="color:red">test</p>
+                </div>
+            </div>
+            <div class="product_container">
+                <img src="./images/booksForHome/2.jpg" class="product_image fade-elements" onmouseenter="showDetails(this)" onmouseleave="hideDetails(this)" />
+                <div class="fade-elements-details">
+                    <p style="color:red">test</p>
+                </div>
+            </div>
+            <div class="product_container">
+                <img src="./images/booksForHome/3.jpg" class="product_image fade-elements" onmouseenter="showDetails(this)" onmouseleave="hideDetails(this)" />
+                <div class="fade-elements-details">
+                    <p style="color:red">test</p>
+                </div>
+            </div>
+            <div class="product_container">
+                <img src="./images/booksForHome/4.jpg" class="product_image fade-elements" onmouseenter="showDetails(this)" onmouseleave="hideDetails(this)" />
+                <div class="fade-elements-details">
+                    <p style="color:red">test</p>
+                </div>
+            </div>
+            <div class="product_container">
+                <img src="./images/booksForHome/5.jpg" class="product_image fade-elements" onmouseenter="showDetails(this)" onmouseleave="hideDetails(this)" />
+                <div class="fade-elements-details">
+                    <p style="color:red">test</p>
+                </div>
+            </div>';
         }
         ?>
     </div>

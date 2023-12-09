ae5f4c964c5ee4d3c9b2a7179e4587f9a45c5fc3
RalphSaridar369 <ralphsaridar@hotmail.com>
2023-12-10 01:38:34 +0200

    updated functionalities php and DataBase

--- checkout.php
+++ checkout.php
@@ -85,18 +85,38 @@
 
     <!-- checkout cart and create order -->
     <?php
-    // if ($_POST['submit_quantity']) {
-    //     if (!isset($_POST['quantity']) || empty($_POST['quantity'])) {
-    //         echo '<script>alert("Please insert quantity");</script>';
-    //     } else {
-    //         $result = mysqli_query($con, "INSERT INTO employee(EmpName, EmpSD, EmpTD, Salary) 
-    //         Values ('$_POST[Ename]','$_POST[SDate]','$_POST[TDate]' ,'$_POST[Salary]')");
+    if (isset($_POST['checkout_submit'])) {
+        if (!isset($_POST['first_name']) || !isset($_POST['last_name']) || !isset($_POST['city']) || !isset($_POST['address']) || !isset($_POST['street']) || !isset($_POST['phone_number'])) {
+            echo '<script>alert("Please fill all fields");</script>';
+        } else {
+            include_once('./actions/connection.php');
+            //get cart's info
 
-    //         if (!$result) {
-    //             echo '<script>alert("Error while inserting book");</script>';
-    //         }
-    //     }
-    // }
+            $result = mysqli_query($con, "SELECT * FROM cart WHERE User_ID = 1 ");
+            $cartRow = mysqli_fetch_assoc($result);
+
+
+            //create order
+
+            $firstName = mysqli_real_escape_string($con, $_POST['first_name']);
+            $lastName = mysqli_real_escape_string($con, $_POST['last_name']);
+            $city = mysqli_real_escape_string($con, $_POST['city']);
+            $address = mysqli_real_escape_string($con, $_POST['address']);
+            $street = mysqli_real_escape_string($con, $_POST['street']);
+            $phoneNum = mysqli_real_escape_string($con, $_POST['phone_number']);
+
+            $cartID = mysqli_real_escape_string($con, $cartRow['ID']);
+
+            $query = "INSERT INTO `order` (First_Name, Last_Name, City, Address, Street, Phone_Num, Cart_ID, User_ID) 
+                      VALUES ('$firstName', '$lastName', '$city', '$address', '$street', '$phoneNum', '$cartID', 1)";
+
+            $result = mysqli_query($con, $query);
+            $orderID = mysqli_insert_id($con);
+
+            // update all line_items
+            $result = mysqli_query($con, "UPDATE line_item SET Order_ID = $orderID WHERE Order_ID IS NULL AND Cart_ID = $cartID ");
+        }
+    }
     ?>
 
 
@@ -109,7 +129,7 @@
             <input type="text" placeholder="Address" name="address" />
             <input type="text" placeholder="Street" name="street" />
             <input type="text" placeholder="Phone Number" name="phone_number" />
-            <input type="submit" class="checkout_submit" />
+            <input type="submit" class="checkout_submit" name="checkout_submit" />
         </form>
         <div class="right_checkout_container">
             <!-- getting line items for customer's cart -->
@@ -117,14 +137,31 @@
             include_once('./actions/connection.php');
 
             $result = mysqli_query($con, "Select * From line_item l, cart c Where l.Order_ID IS NULL AND c.User_ID = 1 ");
+            $total = 0;
+            $items = array(); // Array to store items
+
             if ($result) {
                 while ($row = mysqli_fetch_array($result)) {
-                    echo `
-                    <div class="right_checkout_item">
-                        <img src="./images/booksForHome/1.jpg" alt="image_book" />
-                        <h3>Title</h3>
-                    </div>`;
+                    $total += ($row['Price'] * $row['Quantity']);
+                    $items[] = $row; // Store each row in the items array
                 }
+            }
+
+            echo '<h1 class="total_text">Total: $' . $total . '</h1>';
+
+            foreach ($items as $row) {
+                echo '
+                    <div class="right_checkout_item">
+                        <img src="./images/booksForHome/' . $row['Image_link'] . '" alt="image_book" />
+                        <div class="right_checkout_item_first_container">
+                            <h2>' . $row['Title'] . '</h2>
+                            <div class="quantity_price_container">
+                                <h3>' . $row['Quantity'] . '</h3>
+                                <h4>&nbsp;x&nbsp;$' . $row['Price'] . '</h4>
+                            </div>
+                        </div>
+                        <h3 class="total_text_per_item">Total: $' . ($row['Price'] * $row['Quantity']) . '</h3>
+                    </div>';
             }
             ?>
         </div>

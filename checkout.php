387d696417ce3ce0fe30d8f25881b0f80302483b
RalphSaridar369 <ralphsaridar@hotmail.com>
2023-12-10 20:20:12 +0200

    updated css

--- checkout.php
+++ checkout.php
@@ -12,7 +12,7 @@
     <title>BookHaven</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Esteban&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500&display=swap" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 
     <script>
